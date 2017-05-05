dd99c89a333a0643402633f4a514fe79fbc3a863
AntoninaSavka <antonina.savka@gmail.com>
2017-05-05 22:17:12 +0100

    Add files via upload

---
+++ mail.cpp
@@ -0,0 +1,26 @@
+/*
+ * mail.cpp
+ *
+ *  Created on: 4 May 2017
+ *      Author: asavka
+ */
+
+#include <iostream>
+#include "RectangleSystemProcessor.h"
+
+int main() {
+	try {
+		RectangleSystemProcessor rectSystem("recursive.json");
+		std::cout << "Input: " << std::endl;
+		rectSystem.printInput();
+
+		rectSystem.findIntersections();
+
+		std::cout << "Intersections: " << std::endl;
+		rectSystem.printIntersections();
+	} catch (const std::exception&  e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}

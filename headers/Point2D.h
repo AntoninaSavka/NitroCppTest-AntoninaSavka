bb8e892c232539249fef5ecba50779a186485daf
AntoninaSavka <antonina.savka@gmail.com>
2017-05-05 22:12:57 +0100

    Create Point2D.h

---
+++ headers/Point2D.h
@@ -0,0 +1,34 @@
+/*
+ * Point2D.h
+ *
+ *  Created on: 5 May 2017
+ *      Author: asavka
+ */
+
+#ifndef POINT2D_H_
+#define POINT2D_H_
+
+#include <string>
+
+class Point2D {
+public:
+	Point2D();
+	Point2D(int, int);
+	virtual ~Point2D();
+
+	int getX() const;
+	int getY() const;
+
+	void setX(int);
+	void setY(int);
+
+	std::string toString() const;
+
+	bool operator<(const Point2D&) const;
+
+protected:
+	int x;
+	int y;
+};
+
+#endif /* POINT2D_H_ */
